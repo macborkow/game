86ce177f3b9b94e35ca61b9a95e69cc3f95149fd
Maciej Borkowski <maciej.borkowski@nl.abnamro.com>
2023-02-06 15:51:00 +0100

    add mongodb connection

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gogame
 online go webapp
+
+## favicon
+Copyright 2020 Twitter, Inc and other contributors (https://github.com/twitter/twemoji)
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+  "net/http"
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
+  "context"
+  "log"
   "fmt"
+  "time"
 )
 
 func main() {
-  fmt.Println("Hello world")
+  serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+  clientOptions := options.Client().
+      ApplyURI("mongodb+srv://user:password1234@go.6wehgo4.mongodb.net/?retryWrites=true&w=majority").
+      SetServerAPIOptions(serverAPIOptions)
+  ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+  defer cancel()
+  _, err := mongo.Connect(ctx, clientOptions)
+  if err != nil {
+      log.Fatal(err)
+  }
+  fmt.Println("Success!")
+  fs := http.FileServer(http.Dir("static/"))
+  http.Handle("/", fs)
+
+  http.ListenAndServe(":8080", nil)
 }
