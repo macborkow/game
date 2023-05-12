0ac3fc33df31a96723b85fcb4e048c5e7a05f7e3
Maciej Borkowski <maciej.borkowski@nl.abnamro.com>
2023-05-12 15:07:54 +0200

    add webgl

--- static/code.js
+++ static/code.js
@@ -1,1 +1,91 @@
 document.title = "game"
+let root = document.getElementById("root")
+
+let c = document.createElement("canvas")
+c.style.width = 640
+c.style.height = 480
+c.id = "c"
+root.appendChild(c)
+
+let gl = c.getContext("webgl2")
+if (!gl) {
+  console.log("no webgl for you")
+}
+
+var vertexShaderSource = `#version 300 es
+ 
+// an attribute is an input (in) to a vertex shader.
+// It will receive data from a buffer
+in vec4 a_position;
+ 
+// all shaders have a main function
+void main() {
+ 
+  // gl_Position is a special variable a vertex shader
+  // is responsible for setting
+  gl_Position = a_position;
+}
+`;
+ 
+var fragmentShaderSource = `#version 300 es
+ 
+// fragment shaders don't have a default precision so we need
+// to pick one. highp is a good default. It means "high precision"
+precision highp float;
+ 
+// we need to declare an output for the fragment shader
+out vec4 outColor;
+ 
+void main() {
+  // Just set the output to a constant reddish-purple
+  outColor = vec4(1, 0, 0.5, 1);
+}
+`;
+
+function createShader(gl, type, source) {
+  let shader = gl.createShader(type)
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+  if (success) {
+    return shader
+  }
+
+  console.log(gl.getShaderInfoLog(shader))
+  gl.deleteShader(shader)
+}
+
+let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
+let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  let program = gl.createProgram()
+  gl.attachShader(program, vertexShader)
+  gl.attachShader(program, fragmentShader)
+  gl.linkProgram(program)
+  let success = gl.getProgramParameter(program, gl.LINK_STATUS)
+  if (success) {
+    return program
+  }
+
+  console.log(gl.getProgramInfoLog(program))
+  gl.deleteProgram(program)
+}
+
+let program = createProgram(gl, vertexShader, fragmentShader)
+
+let positionAttributeLocation = gl.getAttributeLocation(program, "a_position")
+let positionBuffer = gl.createBuffer()
+
+let positions = [
+  0, 0,
+  0, 0.5,
+  0.7, 0,
+]
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+let vao = gl.createVertexArray()
+gl.binfVertexArray(vao)
+gl.enableVertexAttribArray(positionAttributeLocation);
+
+gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
+
